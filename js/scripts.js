760e0f3799b057839d6ddaa077a037b3c9abc854
Franklyn R <franklyn1998@live.com>
2022-10-19 15:14:13 -0300

    Inicializacao js

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,26 @@ class BoxShadowGenerator {
         this.webkitRule = webkitRule;
         this.mozRule = mozRule;
     }
+
+    initialize() {
+        this.horizontalRef.value = this.horizontal.value;
+        this.verticalRef.value = this.vertical.value;
+        this.spreadRef.value = this.spread.value;
+        this.blurRef.value = this.blur.value;
+
+        this.applyRule();
+        this.showRule();
+    }
+
+    applyRule() {
+        this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px #000000`;
+        this.currentRule = this.previewBox.style.boxShadow;
+    }
+    showRule() { 
+        this.rule.innerText = `${this.currentRule};`
+        this.webkitRule.innerText = `${this.currentRule};`
+        this.mozRule.innerText = `${this.currentRule};`
+    };
 }
 
 // Selecao de elementos
@@ -58,5 +78,5 @@ const boxShadow = new BoxShadowGenerator(
     webkitRule,
     mozRule
 );
-console.log(boxShadow);
+boxShadow.initialize();
 // Eventos
